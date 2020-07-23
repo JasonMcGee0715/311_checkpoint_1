07511bdb2d365246a81505f7a003e7e93fe512a0
JasonMcGee0715 <jdm0715@gmail.com>
2020-07-23 11:39:33 -0500

    finished GET, GET, and POST - need PUT and DELETE

--- controllers/users.js
+++ controllers/users.js
@@ -1,15 +1,24 @@
 const users = require('../data/index');
+const usersCount = users.length;
 
 const list = (req, res) => {
-return res.json(users);
+    return res.json(users);
 };
 
 const show = (req, res) => {
+    const id = req.params.usersId;
 
+    const foundUser = users.find(user => user.id === Number(id))
+    return res.json(foundUser);
 };
 
 const create = (req, res) => {
-
+    const newUser = {
+        id: usersCount + 1,
+        ...req.body
+    }
+    users.push(newUser);
+    return res.json(users);
 };
 
 const update = (req, res) => {
--- routes/users.js
+++ routes/users.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const usersController = require('../controllers/users');
 
 router.get("/users", usersController.list);
-router.get("/index/:usersId", usersController.show);
-router.post("/index", usersController.create);
+router.get("/users/:usersId", usersController.show);
+router.post("/users", usersController.create);
 router.put("index/:usersId", usersController.update);
 router.delete("index/:usersId", usersController.deleteUser);
 
